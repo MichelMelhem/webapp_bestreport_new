b236d85131218a20053a7e2ff12c0edc11ce1f35
Maamoun Firas <firas.maamoun@gmail.com>
2025-03-28 16:19:37 +0100

    settings page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home.tsx"
-import Account from "./pages/account/Account.tsx"
+import Settings from "@/pages/settings/Settings.tsx"
 import Contact from "./pages/contact/Contact.tsx"
 import NotFound from "@/pages/notfound/NotFound.tsx"
 
@@ -20,7 +20,7 @@ function App() {
         <Suspense fallback={<p className="text-white">Loading...</p>}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/account" element={<Account />} />
+            <Route path="/settings" element={<Settings />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
